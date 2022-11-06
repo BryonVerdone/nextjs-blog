79951bdf56a7b415e57eb5aa286430fbc18b1f9e
bryonverdone <bryonverdone@gmail.com>
2022-11-06 18:58:07 -0500

    able to display post md contents

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,11 @@
 import Layout from '../../components/layout';
-
+import Head from 'next/head';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+
   return {
     props: {
       postData,
@@ -21,11 +23,17 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
+      {/* Add this <Head> tag */}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
       {postData.title}
       <br />
       {postData.id}
       <br />
       {postData.date}
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
