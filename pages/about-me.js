24d0c8b3fa79b5d0835959980e6058122137f845
bryonverdone <bmverdone@gmail.com>
2022-11-07 13:59:46 -0500

    added other pages and links

---
+++ pages/about-me.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+const AboutMe = () => {
+  return (
+    <>
+      <Layout>
+        <div>about-me</div>
+      </Layout>
+    </>
+  );
+};
+
+export default AboutMe;
---
+++ pages/projects.js
@@ -0,0 +1,14 @@
+import Layout from '../components/layout';
+import Nav from '../components/Nav/Nav';
+const Projects = () => {
+  return (
+    <>
+      <Layout>
+        <Nav />
+        <div>Projects</div>
+      </Layout>
+    </>
+  );
+};
+
+export default Projects;
