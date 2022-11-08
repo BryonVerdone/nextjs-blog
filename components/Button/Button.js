3e765405103b10bb5cc5a1f73c9c72e0aeeb0f29
bryonverdone <bmverdone@gmail.com>
2022-11-08 12:00:12 -0500

    installed tailwind

---
+++ components/Button/Button.js
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <button className='bg-orange-900'>Button</button>;
+};
+
+export default Button;
--- pages/about-me.js
+++ pages/about-me.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Layout from '../components/layout';
+import Button from '../components/Button/Button';
 const AboutMe = () => {
   return (
     <>
       <Layout>
+        <Button />
         <div>about-me</div>
       </Layout>
     </>
