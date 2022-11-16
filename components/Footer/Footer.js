df4b3bd2f7c7097c4ddc977ed847bc8bef9f0bc7
bryonverdone <bmverdone@gmail.com>
2022-11-16 12:52:06 -0500

    installed reacted icons,added social icons to footer component

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,19 +1,32 @@
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 const Footer = () => {
   return (
     <footer className='footer footer-center p-10  '>
       <div>
-        <div className='grid grid-flow-col gap-4'>
-          <a target='_blank ' href='https://github.com/BryonVerdone'>
-            Github
+        <div className='text-xl grid grid-flow-col gap-4'>
+          <a
+            className='text-secondary'
+            target='_blank '
+            href='https://github.com/BryonVerdone'
+          >
+            <FaGithub />
           </a>
-          <a href='https://www.linkedin.com/in/bryon-verdone'>Linkedin</a>
-          <a target='_blank' href='https://www.twitter.com/_thebryguy'>
-            Twitter
+          <a
+            className='text-secondary'
+            href='https://www.linkedin.com/in/bryon-verdone'
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            className='text-secondary'
+            target='_blank'
+            href='https://www.twitter.com/_thebryguy'
+          >
+            <FaTwitter />
           </a>
         </div>
       </div>
     </footer>
   );
 };
-
 export default Footer;
